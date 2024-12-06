753dfe21ba91e4ce0695036167f75822e392ab94
sakeththota <sakeththota01@gmail.com>
2024-12-06 13:04:30 -0600

    last input updates

--- src/05-solution.ts
+++ src/05-solution.ts
@@ -21,12 +21,15 @@ export const part1 = async () =>
       if (!rules.has(rule[0])) {
         rules.set(rule[0], new Set());
       }
-      rules.get(rule[0]).add(rule[1]);
+      rules.get(rule[0])!.add(rule[1]);
     }
 
     const validOrder = (update: number[]) => {
       for (let i = 0; i < update.length - 1; ++i) {
-        if (!rules.get(update[i]) || !rules.get(update[i]).has(update[i + 1])) {
+        if (
+          !rules.get(update[i]) ||
+          !rules.get(update[i])!.has(update[i + 1])
+        ) {
           return false;
         }
       }
@@ -38,4 +41,46 @@ export const part1 = async () =>
       .reduce((acc, update) => acc + update[Math.floor(update.length / 2)], 0);
 
     return total;
+  });
+
+export const part2 = async () =>
+  withTiming(async () => {
+    const data = await readInput("05");
+    const [sectionOne, sectionTwo] = data.split("\n\n");
+    const rawOrdering = sectionOne
+      .trim()
+      .split("\n")
+      .map((line) => {
+        const [x, y] = line.split("|");
+        return [parseInt(x, 10), parseInt(y, 10)];
+      });
+    const updates = sectionTwo
+      .trim()
+      .split("\n")
+      .map((line) => line.split(",").map((x) => parseInt(x, 10)));
+
+    const rules = new Map<number, Set<number>>();
+    for (const rule of rawOrdering) {
+      if (!rules.has(rule[0])) {
+        rules.set(rule[0], new Set());
+      }
+      rules.get(rule[0])!.add(rule[1]);
+    }
+
+    const invalidOrder = (update: number[]) => {
+      for (let i = 0; i < update.length - 1; ++i) {
+        if (
+          !rules.get(update[i]) ||
+          !rules.get(update[i])!.has(update[i + 1])
+        ) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    const invalid = updates.filter(invalidOrder);
+    console.log(invalid);
+
+    return 0;
   });
